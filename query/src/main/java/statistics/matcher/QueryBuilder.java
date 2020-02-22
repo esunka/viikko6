0d5c772526e2d32b8615a8d25f79f018074a17f9
antton <antton.kortelainen@gmail.com>
2020-02-22 15:23:16 +0200

    4

---
+++ query/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,30 @@
+package statistics.matcher;
+
+public class QueryBuilder {
+    Matcher matcher;
+    
+    public QueryBuilder() {
+        matcher = new All();
+    }   
+    
+    public Matcher build() {
+        Matcher buildMatcher = this.matcher;
+        this.matcher = new All();
+        return buildMatcher;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }    
+}
